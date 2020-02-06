a327913d9a9f6ea7feb048e9084b31bf5a68f63e
Jovana Novakovic <jovana_novakovic@uns.ac.rs>
2020-02-06 23:08:38 +0100

    services

--- demo/src/main/java/root/demo/services/SendRejectMailToAurthorService.java
+++ demo/src/main/java/root/demo/services/SendRejectMailToAurthorService.java
@@ -1,19 +1,15 @@
 package root.demo.services;
 
-import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class VerifyReviewerService implements JavaDelegate{
-	@Autowired
-	IdentityService identityService;
-	
+public class SendRejectMailToAurthorService  implements JavaDelegate{
+
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-			System.out.println("VerifyReviewerService");
+		System.out.println("SendRejectMailToAurthorService");
 		
 	}
 
--- demo/src/main/java/root/demo/services/SetReviewersService.java
+++ demo/src/main/java/root/demo/services/SetReviewersService.java
@@ -1,19 +1,18 @@
 package root.demo.services;
 
-import org.camunda.bpm.engine.IdentityService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class VerifyReviewerService implements JavaDelegate{
-	@Autowired
-	IdentityService identityService;
-	
+public class SetReviewersService implements JavaDelegate{
+
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-			System.out.println("VerifyReviewerService");
+	/*	var users = ["john", "mary"];
+		var ArrayList = Java.type("java.util.ArrayList");
+		var rec = new ArrayList(users);
+		execution.setVariable("listaRecezenata", rec);*/
 		
 	}
 
