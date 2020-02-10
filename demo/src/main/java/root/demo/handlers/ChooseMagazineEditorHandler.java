9a22759d35d4d076bd38dcbcc3ff513037a98950
Jovana Novakovic <jovana_novakovic@uns.ac.rs>
2020-02-10 02:23:07 +0100

    logic 5

--- demo/src/main/java/root/demo/handlers/ChooseMagazineEditorHandler.java
+++ demo/src/main/java/root/demo/handlers/ChooseMagazineEditorHandler.java
@@ -2,16 +2,22 @@ package root.demo.handlers;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import root.demo.entities.Magazine;
+import root.demo.services.MagazineService;
 
 @Service
 public class ChooseMagazineEditorHandler  implements JavaDelegate {
 
+	@Autowired
+	MagazineService magazineService;
+	
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-		Magazine magazine = (Magazine) execution.getVariable("magazine");
+		Long magazineId = (Long) execution.getVariable("magazine");
+		Magazine magazine = magazineService.getById(magazineId);
 		if(magazine != null) {
 		execution.setVariable("glavniUrednik", magazine.getEditor().getEmail());
 		}
